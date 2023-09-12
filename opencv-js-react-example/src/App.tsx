8fb7c2e510d1d2037969b36845cb90fe516b2974
morninng <mr_morninng_star_dash@yahoo.co.jp>
2023-09-12 18:59:10 +0900

    ss

--- opencv-js-react-example/src/App.tsx
+++ opencv-js-react-example/src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import TestPage from "./opencv/TestPage";
-import { Aaa } from "./mytest/camera-cantour";
+import { CameraCantour } from "./mytest/camera-cantour";
 
 function App() {
   return (
     <div className="App">
       {/* <TestPage /> */}
-      <Aaa />
+      <CameraCantour />
     </div>
   );
 }
