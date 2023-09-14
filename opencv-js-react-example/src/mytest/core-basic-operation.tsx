3f53a3bfe74d35dd5a982d1437da937633578d83
morninng <mr_morninng_star_dash@yahoo.co.jp>
2023-09-14 20:46:59 +0900

    convert pixel color

--- opencv-js-react-example/src/mytest/core-basic-operation.tsx
+++ opencv-js-react-example/src/mytest/core-basic-operation.tsx
@@ -27,9 +27,11 @@ export function CoreBasicOperation() {
           context.drawImage(img, 0, 0, canvasSize.w, canvasSize.h);
           // canvasAnalyze(context);
           setTimeout(()=>{
-            // showMatInfo();
+            showMatInfo();
             // copyMat()
-            convertMat()
+            // convertMat()
+            // executeMatVector()
+            acceessAndModifyPixelValue()
           }, 100);
         }
         img.onerror = function onImageError(err) {
@@ -57,13 +59,15 @@ export function CoreBasicOperation() {
     console.log("canvasAnalyze2");
     if(canvasRef.current){
       const src: Mat =  cv.imread(canvasRef.current); // この
-      console.log('image width: ', src.cols)
-      console.log('src.depth ', src.depth())
-      console.log('src.size() ', src.size())
-      console.log('src.size().width ', src.size().width)
-      console.log('src.size().height ', src.size().height)
-      console.log('src.channels() ', src.channels())
-      console.log('src.type() ', src.type())
+
+      console.log('image rows: ', src.rows)
+      console.log('image cols: ', src.cols)
+      // console.log('src.depth ', src.depth())
+      // console.log('src.size() ', src.size())
+      // console.log('src.size().width ', src.size().width)
+      // console.log('src.size().height ', src.size().height)
+      // console.log('src.channels() ', src.channels())
+      // console.log('src.type() ', src.type())
     }
   }
   const copyMat = () => {
@@ -91,6 +95,58 @@ export function CoreBasicOperation() {
   }
 
 
+  const executeMatVector = () => {
+    if(canvasRef.current){
+      const src: Mat =  cv.imread(canvasRef.current); // この
+
+      // Initialise a MatVector
+      let matVec = new cv.MatVector();
+
+      // MatVector オブジェクトである matVec に src のコピーを追加します。つまり、src に格納されている画像データのコピーが matVec 内に追加される
+      matVec.push_back(src);
+      // Get a Mat fom MatVector
+      let cnt = matVec.get(0);
+      if(convertedCanvasRef.current){
+        cv.imshow(convertedCanvasRef.current, cnt);
+      }
+      src.delete(); matVec.delete(); cnt.delete();
+    }
+  }
+
+
+  const acceessAndModifyPixelValue = () => {
+    let row = 90, col = 90;
+    if(canvasRef.current){
+      const src: Mat =  cv.imread(canvasRef.current); // この
+
+      if (src.isContinuous()) {
+        let R = src.data[row * src.cols * src.channels() + col * src.channels()];
+        let G = src.data[row * src.cols * src.channels() + col * src.channels() + 1];
+        let B = src.data[row * src.cols * src.channels() + col * src.channels() + 2];
+        let A = src.data[row * src.cols * src.channels() + col * src.channels() + 3];
+        console.log(`RGBA(${row}, ${col}) = ( ${R}, ${G}, ${B}, ${A})`);
+      }
+      if(true){
+        let dst = src.clone();
+        let pixel = dst.ucharPtr(row, col);
+        let R = pixel[0];
+        let G = pixel[1];
+        let B = pixel[2];
+        let A = pixel[3];
+        console.log(`RGBA(${row}, ${col}) = ( ${R}, ${G}, ${B}, ${A})`);
+        pixel[0] = 255
+        pixel[1] = 0
+        pixel[2] = 255
+        pixel[3] = 255
+        if(convertedCanvasRef.current){
+          cv.imshow(convertedCanvasRef.current, dst);  // 値を変更したものが描画されている
+        }
+      }
+
+    }
+  }
+
+
   return (
     <div>
 
@@ -98,7 +154,7 @@ export function CoreBasicOperation() {
     original
       <canvas  ref={canvasRef} />
     <br />
-    converted
+    convert
       <canvas  ref={convertedCanvasRef} />
 
 
