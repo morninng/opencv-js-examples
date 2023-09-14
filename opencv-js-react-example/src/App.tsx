fc7a468df0c82d147a7f09e37017ddddd51249ad
morninng <mr_morninng_star_dash@yahoo.co.jp>
2023-09-14 13:29:46 +0900

    base of core base

--- opencv-js-react-example/src/App.tsx
+++ opencv-js-react-example/src/App.tsx
@@ -3,6 +3,7 @@ import TestPage from "./opencv/TestPage";
 import { CameraCantour } from "./mytest/camera-cantour";
 import { RectangleCapture } from "./mytest/rectangle-capture";
 import { ImageTrial } from "./mytest/image-trial";
+import { CoreBasicOperation } from "./mytest/core-basic-operation";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <TestPage /> */}
       {/* <CameraCantour /> */}
       {/* <RectangleCapture /> */}
-      <ImageTrial />
+      {/* <ImageTrial /> */}
+      <CoreBasicOperation />
     </div>
   );
 }
---
+++ opencv-js-react-example/src/mytest/core-basic-operation.tsx
@@ -0,0 +1,94 @@
+
+import React, {useEffect, useRef, useState} from 'react';
+import cv, { set } from "@techstark/opencv-js";
+
+const canvasSize = { w: 360, h: 240 };
+
+
+export function CoreBasicOperation() {
+
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const convertedCanvasRef = useRef<HTMLCanvasElement>(null)
+
+  useEffect(()=>{
+    console.log("useEffect");
+
+
+    if(canvasRef.current){
+      canvasRef.current.id     = 'canvas';
+      canvasRef.current.width  = canvasSize.w;
+      canvasRef.current.height = canvasSize.h;
+      const context = canvasRef.current.getContext('2d');
+      if(context){
+        var img = new Image();
+
+        img.src = './receipt2.png';
+        img.onload = function onImageLoad() {
+          context.drawImage(img, 0, 0, canvasSize.w, canvasSize.h);
+          // canvasAnalyze(context);
+          setTimeout(()=>{
+            canvasAnalyze2();
+          }, 100);
+        }
+        img.onerror = function onImageError(err) {
+          console.log("ereror", err)
+        }
+      }
+
+    }else{
+      console.log("canvasRef.current is null");
+    }
+
+    if(convertedCanvasRef.current)  {
+      convertedCanvasRef.current.width  = canvasSize.w;
+      convertedCanvasRef.current.height = canvasSize.h;
+    }else{
+      console.log("convertedCanvasRef.current is null");
+    }
+
+
+
+
+  }, [])
+
+  const canvasAnalyze2 = () => {
+    console.log("canvasAnalyze2");
+    if(canvasRef.current){
+      const src =  cv.imread(canvasRef.current);
+      let dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
+      cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
+      cv.threshold(src, src, 177, 200, cv.THRESH_BINARY);
+      let contours = new cv.MatVector();
+      let hierarchy = new cv.Mat();
+      cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+
+      const edges = new cv.Mat();
+      src.copyTo(edges);
+
+      if(convertedCanvasRef.current){
+        cv.imshow(convertedCanvasRef.current, edges);
+      }
+      src.delete();
+      dst.delete();
+      edges.delete();
+      contours.delete();
+      hierarchy.delete();
+
+    }
+  }
+
+
+  return (
+    <div>
+
+    <br />
+    original
+      <canvas  ref={canvasRef} />
+    <br />
+    converted
+      <canvas  ref={convertedCanvasRef} />
+
+
+    </div>
+  )
+}
