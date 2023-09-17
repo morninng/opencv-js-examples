da673807b790e891e26f7ca569e0a3b1916c20d4
morninng <mr_morninng_star_dash@yahoo.co.jp>
2023-09-17 11:24:08 +0900

    perspective transformation

--- opencv-js-react-example/src/mytest/image-processing.tsx
+++ opencv-js-react-example/src/mytest/image-processing.tsx
@@ -52,7 +52,8 @@ export function ImageProcessing() {
             // resizeImage()
             // affineTransition()
             // affineTransitionRotate()
-            affineTransform()
+            // affineTransform()
+            perspectiveTransformation()
 
           }, 100);
         }
@@ -152,6 +153,28 @@ export function ImageProcessing() {
       let dsize = new cv.Size(src.rows, src.cols);
       let M = cv.getAffineTransform(srcTri, dstTri) // 変換前と変換後の三角形の頂点座標からアフィン変換の変換行列を計算します
       cv.warpAffine(src, dst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar()); // アフィン変換を適用
+      if(convertedCanvasRef.current){
+        cv.imshow(convertedCanvasRef.current, dst);
+      }
+      src.delete(); dst.delete(); M.delete(); srcTri.delete(); dstTri.delete();
+    }
+  }
+
+  const perspectiveTransformation = () => {
+    if(canvasRef.current){
+      const src: Mat =  cv.imread(canvasRef.current);
+
+      let dst = new cv.Mat();
+      let dsize = new cv.Size(src.rows, src.cols);
+      // (data32F[0], data32F[1]) is the first point
+      // (data32F[2], data32F[3]) is the sescond point
+      // (data32F[4], data32F[5]) is the third point
+      // (data32F[6], data32F[7]) is the fourth point
+      let srcTri = cv.matFromArray(4, 1, cv.CV_32FC2, [56, 65, 368, 52, 28, 587, 389, 390]);
+      let dstTri = cv.matFromArray(4, 1, cv.CV_32FC2, [0, 0, 300, 0, 0, 300, 200, 300]);
+      let M = cv.getPerspectiveTransform(srcTri, dstTri);
+      // You can try more different parameters
+      cv.warpPerspective(src, dst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
       if(convertedCanvasRef.current){
         cv.imshow(convertedCanvasRef.current, dst);
       }
