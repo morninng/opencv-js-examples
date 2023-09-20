8ab52edd7537b17e812396a3c80c1625ac2a06a4
morninng <mr_morninng_star_dash@yahoo.co.jp>
2023-09-20 10:48:32 +0900

    copy contourApproxPolyDP to namecard.tsx

--- opencv-js-react-example/src/mytest/name-card.tsx
+++ opencv-js-react-example/src/mytest/name-card.tsx
@@ -111,7 +111,37 @@ export function NameCard() {
       }
 
 
+  const contourApproxPolyDP = () => {
+    if(canvasRef.current){
+      const src: Mat =  cv.imread(canvasRef.current);
+
+      let dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
+      cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
+      cv.threshold(src, src, 100, 200, cv.THRESH_BINARY);
+      let contours = new cv.MatVector();
+      let hierarchy = new cv.Mat();
+      let poly = new cv.MatVector();
+      cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+      // approximates each contour to polygon
+
+      const size = contours.size() as unknown as number
+      for (let i = 0; i < size; ++i) {
+        let tmp = new cv.Mat();
+        let cnt = contours.get(i);
+        // You can try more different parameters
+        cv.approxPolyDP(cnt, tmp, 3, true);
+        poly.push_back(tmp);
+        cnt.delete(); tmp.delete();
+      }
+      // draw contours with random Scalar
+      for (let i = 0; i < size; ++i) {
+        let color = new cv.Scalar(255, 0, 0);
+        console.log('drawContours')
+        cv.drawContours(dst, poly, i, color, 1, 8, hierarchy, 0);
+      }
+
       if(convertedCanvasRef.current){
+        console.log('imshow')
         cv.imshow(convertedCanvasRef.current, dst);
       }
     }
